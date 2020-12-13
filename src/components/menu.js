56e7df9c9912ab2ee3af449b5c1f8f517364af56
0racularGh0st <nigel4tariang@gmail.com>
2020-12-13 20:59:53 +0530

    initialized with gatsby default starter and added scss support

---
+++ src/components/menu.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+const Menu = () => {
+    return(
+        <div>
+            <Link to="/About">About</Link>
+        </div>
+    )
+}
+export default Menu;
