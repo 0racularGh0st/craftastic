8bc11f92c55c343179b7a0b423ca15439d289e03
0racularGh0st <nigel4tariang@gmail.com>
2020-12-14 11:52:32 +0530

    Test out parallax effect

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,18 @@
-import React from "react"
+import React, {useEffect} from "react"
 // import { Link } from "gatsby"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-
+import "./main.scss";
 const IndexPage = () => {
+  useEffect(()=>{
+    window.addEventListener('scroll', ()=>{
+      const parallax = document.querySelector('.background-main-style');
+      let scrollPosition = window.pageYOffset;
+      parallax.style.transform = "translateY("+ scrollPosition*0.5 + "px)";
+    })
+  });
   const data = useStaticQuery(graphql`
   query {
     placeholderImage: file(relativePath: { eq: "background-main.jpg" }) {
@@ -20,8 +27,12 @@ const IndexPage = () => {
 return (
   <Layout>
     <SEO title="Home" />
-    <Img fluid={data.placeholderImage.childImageSharp.fluid} fadeIn durationFadeIn={1000}/>
-    <h1>Welcome to Craftastic!</h1>
+    <Img fluid={data.placeholderImage.childImageSharp.fluid} 
+    fadeIn 
+    durationFadeIn={1000}
+    className="background-main-style"
+    />
+    <h1 style={{height:"150vh",marginTop:"50vh"}}>Welcome to Craftastic!</h1>
     <div>
     </div>
   </Layout>
