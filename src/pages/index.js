909b6806a6b9a701bffaeb3230a2f0d472a42d93
0racularGh0st <nigel4tariang@gmail.com>
2020-12-14 23:46:40 +0530

    testing parallax effect

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,13 +6,15 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import "./main.scss";
 const IndexPage = () => {
-  // useEffect(()=>{
-  //   window.addEventListener('scroll', ()=>{
-  //     const parallax = document.querySelector('.background-main-style');
-  //     let scrollPosition = window.pageYOffset;
-  //     parallax.style.transform = "translateY("+ scrollPosition*0.5 + "px)";
-  //   })
-  // });
+  useEffect(()=>{
+    window.addEventListener('scroll', ()=>{
+      const parallax = document.querySelectorAll('.background-main-style');
+      let scrollPosition = window.pageYOffset;
+      parallax.forEach(prlx => {
+        prlx.style.transform = "translateY("+ scrollPosition*0.6 + "px)";
+      })
+    })
+  });
   const data = useStaticQuery(graphql`
   query {
     placeholderImage: file(relativePath: { eq: "background-main.jpg" }) {
@@ -27,6 +29,22 @@ const IndexPage = () => {
 return (
   <Layout>
     <SEO title="Home" />
+    <Img fluid={data.placeholderImage.childImageSharp.fluid} 
+    fadeIn 
+    durationFadeIn={1000}
+    className="background-main-style"
+    />
+    <div className="home-text-style">
+      <h1>Welcome to Craftastic!</h1>
+    </div>
+    <Img fluid={data.placeholderImage.childImageSharp.fluid} 
+    fadeIn 
+    durationFadeIn={1000}
+    className="background-main-style"
+    />
+    <div className="home-text-style">
+      <h1>Welcome to Craftastic!</h1>
+    </div>
     <Img fluid={data.placeholderImage.childImageSharp.fluid} 
     fadeIn 
     durationFadeIn={1000}
