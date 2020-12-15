901740d36d41500a3915d4d8b6edde0073c053e8
0racularGh0st <nigel4tariang@gmail.com>
2020-12-16 00:06:29 +0530

    get parallax effect using rellax.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,31 @@ import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Rellax from 'rellax';
 import "./main.scss";
 const IndexPage = () => {
-  useEffect(()=>{
-    window.addEventListener('scroll', ()=>{
-      const parallax = document.querySelector('.background-main-style');
-      let scrollPosition = window.pageYOffset;
-        parallax.style.transform = "translateY("+ scrollPosition*0.5 + "px)";
-  });
-})
+//   useEffect(()=>{
+//     window.addEventListener('scroll', ()=>{
+//       const parallax = document.querySelector('.background-main-style');
+//       let scrollPosition = window.pageYOffset;
+//         parallax.style.transform = "translateY("+ scrollPosition*0.3 + "px)";
+//   });
+// })
+  useEffect(()=> {
+    window.scrollTo(0,0);
+    const rellax = new Rellax(".background-main-style",{
+      speed: -4,
+    center: true,
+    wrapper: null,
+    round: true,
+    vertical: true,
+    horizontal: false
+    });
+    return() => {
+      console.log("cleanup");
+      rellax.destroy();
+    }
+  })
   const data = useStaticQuery(graphql`
   query {
     placeholderImage: file(relativePath: { eq: "background-main.jpg" }) {
@@ -30,7 +46,23 @@ return (
     <Img fluid={data.placeholderImage.childImageSharp.fluid} 
     fadeIn 
     durationFadeIn={1000}
-    className="background-main-style"
+    className="rellax background-main-style"
+    />
+    <div className="home-text-style">
+      <h1>Welcome to Craftastic!</h1>
+    </div>
+    <Img fluid={data.placeholderImage.childImageSharp.fluid} 
+    fadeIn 
+    durationFadeIn={1000}
+    className="rellax background-main-style"
+    />
+    <div className="home-text-style">
+      <h1>Welcome to Craftastic!</h1>
+    </div>
+    <Img fluid={data.placeholderImage.childImageSharp.fluid} 
+    fadeIn 
+    durationFadeIn={1000}
+    className="rellax background-main-style"
     />
     <div className="home-text-style">
       <h1>Welcome to Craftastic!</h1>
