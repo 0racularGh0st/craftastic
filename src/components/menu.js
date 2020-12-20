ea725ae34799052ceab5a42c5529f68c803d62fe
0racularGh0st <nigel4tariang@gmail.com>
2020-12-20 21:38:56 +0530

    Add burger menu for mobile view

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Link } from 'gatsby';
 import "./menu.scss";
 import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
@@ -6,24 +6,41 @@ import PhotoCameraRoundedIcon from '@material-ui/icons/PhotoCameraRounded';
 import InfoRoundedIcon from '@material-ui/icons/InfoRounded';
 import PermPhoneMsgRoundedIcon from '@material-ui/icons/PermPhoneMsgRounded';
 const Menu = () => {
+    useEffect(()=>{
+        const burger = document.querySelector(".burger");
+        const menu = document.querySelector(".menu-wrapper");
+        burger.addEventListener("click", () => {
+            menu.classList.toggle("show");
+            //burger animation
+            console.log("Clicked");
+            burger.classList.toggle("toggle");
+          });
+    })
     return(
+        <div>
+            <div className="burger">
+                <div className="line1"></div>
+                <div className="line2"></div>
+                <div className="line3"></div>
+            </div>
         <div className="menu-wrapper">
             <Link to="/" className="menu-link-styles">
-                <HomeRoundedIcon style={{color:"white",fontSize:"18px"}}/>
+                <HomeRoundedIcon className="menu-item-style"/>
                 <h4>Home</h4>
             </Link>
             <Link to="/Gallery" className="menu-link-styles">
-                <PhotoCameraRoundedIcon style={{color:"white",fontSize:"18px"}}/>
+                <PhotoCameraRoundedIcon className="menu-item-style"/>
                 <h4>Gallery</h4>
             </Link>
             <Link to="/About" className="menu-link-styles">
-                <InfoRoundedIcon style={{color:"white",fontSize:"18px"}}/>
+                <InfoRoundedIcon className="menu-item-style"/>
                 <h4>About</h4>
             </Link>
             <Link to="/Contact" className="menu-link-styles">
-                <PermPhoneMsgRoundedIcon style={{color:"white",fontSize:"18px"}}/>
+                <PermPhoneMsgRoundedIcon className="menu-item-style"/>
                 <h4>Contact</h4>
             </Link>
+        </div>
         </div>
     )
 }
